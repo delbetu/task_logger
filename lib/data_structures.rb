8fdb1e8122baf0d9768b5f530da947a570e4ab43
M.Bellucci <marcos@tarmac.io>
2016-08-14 23:54:51 -0300

    Allow entry struct to be created from a hash

--- lib/data_structures.rb
+++ lib/data_structures.rb
@@ -1,2 +1,13 @@
-Entry = Struct.new(:id, :date, :startime, :endtime,
+class Entry < Struct.new(:id, :date, :start_time, :end_time,
                    :project, :category, :description)
+
+  def initialize(hash_values)
+    self.id = hash_values[:id]
+    self.date = hash_values[:date]
+    self.start_time = hash_values[:start_time]
+    self.end_time = hash_values[:end_time]
+    self.project = hash_values[:project]
+    self.category = hash_values[:category]
+    self.description = hash_values[:description]
+  end
+end
